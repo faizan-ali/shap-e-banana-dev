8799813173c460cb505ae8a46ac54e841ce96699
Faizan Ali <faizanali619@gmail.com>
2023-09-06 20:03:11 -0700

    add device to download.py

--- download.py
+++ download.py
@@ -1,10 +1,11 @@
 from shap_e.models.download import load_model
-
+import torch
 
 # Runs during container build time to get model weights built into the container
 def download_model():
-    xm = load_model('transmitter')
-    model = load_model('text300M')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    load_model('transmitter', device=device)
+    load_model('text300M', device=device)
 
 
 if __name__ == "__main__":
